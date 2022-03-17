e8da0960814930edd00fae224362da581439ef38
BrktrL <202106045@ogrenci.yalova.edu.tr>
2022-03-17 15:38:26 +0300

    fix list favorite serializer

--- core/FavoritesApp/api/serializers.py
+++ core/FavoritesApp/api/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from FavoritesApp.models import ModelFavorite
+from ProductsApp.api.serializers import ProductsSerializer
+
 
 class FavoriteSerializer(serializers.ModelSerializer):
+    product=ProductsSerializer()
     class Meta:
         model  =ModelFavorite
-        fields =("product",)
+        fields =("user","product")
